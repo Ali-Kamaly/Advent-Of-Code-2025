35be4379424ac8ed0fdf149ddf65534969bd9277
Ali Kamaly <alikamaly07@gmail.com>
2025-12-06 17:17:58 +0000

    Solve Day 5 Part 2

--- day5.py
+++ day5.py
@@ -1,6 +1,5 @@
 def read_file():
     fresh_range = []
-    ingredient_data = []
     next_data = False
     with open("day5.txt") as file:
         lines = file.readlines()
@@ -9,11 +8,11 @@ def read_file():
                 next_data = True
             else:
                 if next_data:
-                    ingredient_data.append(int(line))
+                    pass
                 else:
                     fresh_range.append(line)
 
-    return fresh_range, ingredient_data
+    return fresh_range
 
 def find_ranges(data):
     new_data = []
@@ -23,27 +22,52 @@ def find_ranges(data):
         
         new_data.append((lower_bound, upper_bound))
     return new_data
-        
-def check_fresh(fresh_range, ingredient_data):
-    total_fresh = 0
-    fresh_found = False
-    for food_id in ingredient_data:
-        fresh_found = False
-        if not fresh_found:
-            for i in range(len(fresh_range)):
-                lower_bound, upper_bound = fresh_range[i]
-                if int(lower_bound)<=food_id<=int(upper_bound):
-                    total_fresh+=1
-                    fresh_found = True
+
+def simplify_intervals(data):
+    new_data = []
+    print("data: ", data)
+    try:
+        for _ in range(len(data)):
+    
+            try:
+                smallest, up_limit = min(data)
+            except ValueError:
+                break
+            loc = data.index((smallest, up_limit))
+            data.pop(loc)
+            current_start = smallest
+            current_end = up_limit
+
+            while data:
+                try:
+                    smaller, upper_lim = min(data)
+                except ValueError:
                     break
-    return total_fresh
+
+                if smaller <= current_end + 1:
+                    current_end = max(upper_lim, current_end)
+                    data.pop(data.index((smaller,upper_lim)))
+                else:
+                    break
+            new_data.append((current_start, current_end))
+
+    except IndexError:
+        pass
+
+    return (new_data)
+
+def total_num(data):
+    total = 0
+    for i in range(len(data)):
+        lb, ub = data[i]
+        total += (ub-lb)+1
+    return total
 
 def main():
-    fresh_range, ingredient_data = read_file()
+    fresh_range = read_file()
     fresh_range = find_ranges(fresh_range)
-    print("Fresh-range: ", fresh_range)
-
-    print(check_fresh(fresh_range, ingredient_data))
+    simplified_intervals = simplify_intervals(fresh_range)
+    print(total_num(simplified_intervals))
 
 if __name__ == "__main__":
     main()
